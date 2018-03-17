d56da417cfd1b2379e8dad2af73cfc5e805bce4e
ShawnSwinsick <vifs888@gmail.com>
2018-03-17 17:10:34 -0600

    Real first commit

---
+++ ArrayExample/ArrayExample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            //pretty much every array, collections, whatever, in C# are 0 based
+            new Array();
+            Console.ReadLine();
+            MultiDimensionalArray.TwoDArray();
+            Console.ReadLine();
+            MultiDimensionalArray.ThreeDArray();
+            Console.ReadLine();
+            new DynamicArray();
+            Console.ReadLine();
+        }
+    }
+}
